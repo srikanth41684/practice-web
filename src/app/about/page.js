99603666ab3ea9eac04186a222b97cb7b7d295f0
Usnagiri Srikanth <UsnagiriS@jmf.com>
2023-07-12 14:30:18 +0530

    about us

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,7 +3,27 @@ import React from "react";
 const About = () => {
   return (
     <div className="px-5 max-w-[1400px] m-auto">
-      <h1>About</h1>
+      <div className="pt-5">
+        <h1 className="text-4xl pb-5">About Us</h1>
+        <div className="text-xl pb-5">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </div>
+        <div className="text-xl">
+          The standard chunk of Lorem Ipsum used since the 1500s is reproduced
+          below for those interested. Sections 1.10.32 and 1.10.33 from "de
+          Finibus Bonorum et Malorum" by Cicero are also reproduced in their
+          exact original form, accompanied by English versions from the 1914
+          translation by H. Rackham.
+        </div>
+      </div>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,27 @@
 import React from "react";
 
-const Dashboard = () => {
+export async function getTodosData() {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/todos`);
+  return response.json();
+}
+
+const Dashboard = async () => {
+  const data = await getTodosData();
+
   return (
     <div className="px-5 max-w-[1400px] m-auto">
-      <h1>Dashboard</h1>
+      <div className="pt-5">
+        <h1 className="text-4xl pb-5">Todos Data</h1>
+        <div>
+          {data.map((item, index) => {
+            return (
+              <div>
+                <div className="capitalize py-2">{item.title}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };

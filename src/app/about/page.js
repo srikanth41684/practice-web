c6143d296778bdc3d7ba6f5f3540323d9a367250
Usnagiri Srikanth <UsnagiriS@jmf.com>
2023-07-13 10:45:16 +0530

    git add

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -21,7 +21,8 @@ const About = () => {
           below for those interested. Sections 1.10.32 and 1.10.33 from "de
           Finibus Bonorum et Malorum" by Cicero are also reproduced in their
           exact original form, accompanied by English versions from the 1914
-          translation by H. Rackham.
+          translation by H. Rackham. exact original form, accompanied by English
+          versions from the 1914 translation by H. Rackham.
         </div>
       </div>
     </div>
